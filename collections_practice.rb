3d7c785998af3c2a0444597e993572c8f18627e9
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-26 22:52:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  kesha_array = Array.new
+  array.each do |word|
+    word[2] = "$"
+    kesha_array << word
+  end
+  kesha_array
+end
